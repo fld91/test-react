e717cc21df578516d8b01c684c9a82b0049d521f
harry91 <shriharipardhikar123@gmail.com>
2021-09-29 14:29:00 +0530

    BasicFunctionalComponent in React using JSX

---
+++ src/NavBar.js
@@ -0,0 +1,13 @@
+
+
+ function NavBar(){
+    return(
+        <h2>this is from Navbar</h2>
+    );
+}
+//** */ this is syntax to write function based component  */
+
+export default NavBar;
+
+
+//note :this is export default
